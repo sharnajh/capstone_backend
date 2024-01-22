bce15c267955aaf7a614e35c4e235396315b8bb4
Sharna Hossain <sharnajh@gmail.com>
2024-01-21 19:06:43 -0500

    update gitignore

--- src/test/java/com/yearup/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/yearup/capstone/CapstoneApplicationTests.java
@@ -5,11 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-		"spring.datasource.url=jdbc:mysql://database1.chik2ww0m0iy.us-east-1.rds.amazonaws.com:3306/capstone",
-		"spring.datasource.username=sharnajh",
-		"spring.datasource.password=zi6<1T>%XryW]BY#R-8K$Bxfc#0x"
-})
 class CapstoneApplicationTests {
 
 	@Test
